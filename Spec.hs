86980d56b8e6410f10ff1c3f455a4550bd892509
Søren Palmund <palmund@gmail.com>
2014-11-06 18:02:08 +0100

    Added more descriptive functions to Spec.

--- Spec.hs
+++ Spec.hs
@@ -4,13 +4,24 @@ import Path
 correct = Node 1 (Node 3 (Node 5 (Node 8 (EndNode 12) (EndNode 13)) (Node 9 (EndNode 13) (EndNode 14))) (Node 6 (Node 9 (EndNode 13) (EndNode 14)) (Node 10 (EndNode 14) (EndNode 15)))) (Node 4 (Node 6 (Node 9 (EndNode 13) (EndNode 14)) (Node 10 (EndNode 14) (EndNode 15))) (Node 7 (Node 10 (EndNode 14) (EndNode 15)) (Node 11 (EndNode 15) (EndNode 16))))
 test2 = Node 0 (Node 3 (Node 5 (EndNode 8) (EndNode 9)) (Node 6 (EndNode 9) (EndNode 0))) (Node 4 (Node 6 (EndNode 9) (EndNode 0)) (Node 7 (EndNode 0) (EndNode 1)))
 
+it :: String -> Bool -> IO ()
+it description io = putStrLn ("  - " ++ description ++ ":  " ++ show io)
+
+describe :: String -> IO () -> IO ()
+describe name actions = putStrLn name >> actions
+
 main :: IO ()
 main = do
-  putStr "Build complex tree:\t"
-  putStrLn $ show (znip 1 [3,4] [[5,6,7],[8,9,10,11],[12,13,14,15,16]] == correct)
-  putStr "Build simple tree:\t"
-  putStrLn $ show (znip 0 [3,4] [[5,6,7],[8,9,0,1]] == test2)
-  putStr "Build complex tree (Path):\t"
-  putStrLn $ show (mkTree [[1],[3,4],[5,6,7],[8,9,10,11],[12,13,14,15,16]] == correct)
-  putStr "Build simple tree (Path):\t"
-  putStrLn $ show (mkTree [[0],[3,4],[5,6,7],[8,9,0,1]] == test2)
+  describe "Snip.znip" $ do
+    it "should build complex tree" $ do
+      znip 1 [3,4] [[5,6,7],[8,9,10,11],[12,13,14,15,16]] == correct
+
+    it "should build simple tree" $ do
+      znip 0 [3,4] [[5,6,7],[8,9,0,1]] == test2
+
+  describe "Path.mkTree" $ do
+    it "should build a complex tree" $ do
+      mkTree [[1],[3,4],[5,6,7],[8,9,10,11],[12,13,14,15,16]] == correct
+      
+    it "should build a simple tree" $ do
+      mkTree [[0],[3,4],[5,6,7],[8,9,0,1]] == test2
