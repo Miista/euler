a7a4393e49efdb1dc7ba76efa372befab4f65962
Søren Palmund <palmund@gmail.com>
2014-11-06 17:40:18 +0100

    Added Path.

---
+++ Path.hs
@@ -0,0 +1,14 @@
+module Path where
+  import Data.List
+  import Snip
+
+  mkTree :: [[a]] -> Node a
+  mkTree list = 
+    let root = head $ head list
+        children = head $ tail list
+        rest = if length list < 3
+                then []
+                else tail $ tail list
+    in znip root children rest
+  
+  p1 = mkTree [[0],[3,4],[5,6,7],[8,9,10,11]] --znip 0 [3,4] [[5,6,7],[8,9,0,1]]
