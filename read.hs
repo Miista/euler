2237737572699878adf863b51a722547e0893083
Søren Palmund <palmund@gmail.com>
2014-11-07 15:57:33 +0100

    Extracted lambdas into functions.

--- read.hs
+++ read.hs
@@ -1,23 +1,13 @@
+import Path
+
+stringToInt :: String -> Int
+stringToInt s = read s :: Int
+
+stringToIntList :: String -> [Int]
+stringToIntList l = map stringToInt $ words l
+
 main = do
-  -- contents <- readFile "data.txt"
-  -- let l = lines contents
-  -- let l1 = map words l
-  -- let l2 = words $ head l
-  --let l2 = map (\x -> read x :: [Char]) l
-  -- putStrLn $ show (read (head l2) :: Int)
-
-  -- let contents = "1\n2 3"
-  -- let l = lines contents --"1 2 3"
-  -- let w = words l
-  -- let wm = map (\x -> read x :: Int)
-  -- let l2 = read . head $ words l :: Int
-  -- putStrLn $ show $ wm w
-
   contents <- readFile "data.txt" --lines "1\n2 3"
-  let input = lines contents
-  let literalTree = map (\x -> map (\y -> read y :: Int) (words x)) input
-  putStrLn $ show literalTree
-
-  -- input = words "1 2"
-  -- map (\x -> read x :: Int) [input]
-  -- map (\x -> map (\y -> read y :: Int) (words x)) input
+  let literalTree = map stringToIntList $ lines contents
+  let tree = Path.mkTree literalTree
+  putStrLn . show $ Path.findMaxSum tree
